8fa2d6d3d53bb9ad9745569b0bdb24fad2e9cf94
leadiville <leadivilletech@gmail.com>
2025-06-03 17:14:23 +0100

    feat(features-section): add features section to homepage

---
+++ src/app/components/homePage/AboutUs.tsx
@@ -0,0 +1,80 @@
+import Image from 'next/image'
+import React from 'react'
+
+const AboutUs = () => {
+    return (
+        <div>
+            <div className="container-fluid bg-light about pb-5">
+                <div className="container pb-5">
+                    <div className="row g-5">
+                        <div className="col-xl-6 wow fadeInLeft " data-wow-delay="0.2s">
+                            <div className="about-item-content bg-white rounded p-5 h-100 d-flex align-items-center">
+                                <div>
+                                    <h4 className="text-primary">About Avataworks</h4>
+                                    <h1 className="display-4 mb-4 text-capitalize">Your All-in-One Digital Team—Powered by AVATAs</h1>
+                                    <p className='text-black-50'>From the comfort of your home or office, you can access a full team of remote digital experts—your personal AVATAs (Advanced Virtual Assistants in Technology and Advertising)—dedicated to helping you scale with ease.
+                                    </p>
+                                    <p className='text-black-50'>Whether you are an ambitious startup in Africa or a thriving brand aiming for global relevance, AVATAWorks is your all-in-one partner for digital brand success. We combine strategic branding, tech-driven creativity, and result-focused execution to deliver powerful marketing, design, and business growth solutions.
+                                    </p>
+                                    {/* <p className="text-dark"><i className="fa fa-check text-primary me-3"></i>We can save your money.</p>
+                                    <p className="text-dark"><i className="fa fa-check text-primary me-3"></i>Speed up production</p>
+                                    <p className="text-dark mb-4"><i className="fa fa-check text-primary me-3"></i>Our Our plans are flexible</p> */}
+                                    <a className="btn btn-primary rounded-pill py-3 px-5 text-capitalize" href="#">meet the avatas</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-6 wow fadeInRight" data-wow-delay="0.2s">
+                            <div className="bg-white rounded p-5 h-100">
+                                <div className="row g-4 justify-content-center">
+                                    <div className="col-12">
+                                        <div className="rounded bg-light">
+                                            <Image width={800} height={600} src="/Cross-platform software-pana.png" className="img-fluid w-75 h-50" alt="about-us-image" />
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <div className="counter-item bg-light rounded p-3 h-100">
+                                            <div className="counter-counting">
+                                                <span className="text-primary fs-2 fw-bold" data-toggle="counter-up">50</span>
+                                                <span className="h1 fw-bold text-primary">+</span>
+                                            </div>
+                                            <h4 className="mb-0 text-dark">Businesses Managed</h4>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <div className="counter-item bg-light rounded p-3 h-100">
+                                            <div className="counter-counting">
+                                                <span className="text-primary fs-2 fw-bold" data-toggle="counter-up">30</span>
+                                                <span className="h1 fw-bold text-primary">+</span>
+                                            </div>
+                                            <h4 className="mb-0 text-dark">Skilled Agents</h4>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <div className="counter-item bg-light rounded p-3 h-100">
+                                            <div className="counter-counting">
+                                                <span className="text-primary fs-2 fw-bold" data-toggle="counter-up">5</span>
+                                                <span className="h1 fw-bold text-primary">+</span>
+                                            </div>
+                                            <h4 className="mb-0 text-dark">Success Years</h4>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <div className="counter-item bg-light rounded p-3 h-100">
+                                            <div className="counter-counting">
+                                                <span className="text-primary fs-2 fw-bold" data-toggle="counter-up">20</span>
+                                                <span className="h1 fw-bold text-primary">+</span>
+                                            </div>
+                                            <h4 className="mb-0 text-dark">Team Members</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutUs
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-import Image from "next/image";
-// import styles from "./page.module.css";
 
 import Navbar from "./components/Navbar";
 import { HeroCarousel } from "../../carousels/BootstrapCarousel";
+import Features from "./components/homePage/Features";
+import AboutUs from "./components/homePage/AboutUs";
 
-export default function Home() {
+export default async function Home() {
+
+  const request = await fetch('http://localhost:3000/database');
+  const data = await request.json();
+  const { services, features } = data;
+
   return (
     <>
       <Navbar />
       <HeroCarousel />
+      <Features featuresData={features} />
+      <AboutUs />
     </>
   )
 }
