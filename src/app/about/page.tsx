9f8a8c32d4fff69c2bce627bddec3d32390930f1
leadiville <leadivilletech@gmail.com>
2025-07-12 01:52:53 +0100

    add images (testimonial) homepage and about us

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,6 +5,7 @@ import AboutUs from "../components/homePage/AboutUs"
 import Faq from "../components/homePage/Faq"
 import Team from "../components/homePage/Team"
 import Footer from "../components/Footer"
+// import AboutHero from "../components/AboutUs/AboutHero"
 
 export default async function () {
 
@@ -16,20 +17,9 @@ export default async function () {
     return (
         <>
             <Navbar />
-            {/* hero section */}
-            <div className="container-fluid bg-breadcrumb">
-                <div className="container text-center py-5" style={{ maxWidth: "900px" }}>
-                    <h4 className="text-primary display-4 mb-4 wow fadeInDown" data-wow-delay="0.1s">About Avataworks</h4>
-                    <ol className="breadcrumb d-flex justify-content-center mb-0 wow fadeInDown" data-wow-delay="0.3s">
-                        <li className="breadcrumb-item text-warning text-danger"><a href="#">Get a quote</a></li>
-                        <li className="breadcrumb-item active">Speak to an avata</li>
-                    </ol>
-                </div>
-            </div>
             <AboutUs />
             <Features featuresData={features} title={'At Avataworks, we ensure digital brand success'} />
             <Faq faqs={faq.servicePage} />
-            {/* contact us */}
             <Team teamMembers={ourTeam} />
             <Footer />
         </>
