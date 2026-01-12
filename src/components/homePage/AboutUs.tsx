9573da15b3ea852c1fa858d5ad91db0688d6bcac
leadiville <leadivilletech@gmail.com>
2026-01-12 17:45:14 +0100

    style: Features page and about us modified

--- src/components/homePage/AboutUs.tsx
+++ src/components/homePage/AboutUs.tsx
@@ -10,28 +10,21 @@ const AboutUs = () => {
                 <div className="container pb-5">
                     <div className="row g-5">
                         <div className="col-xl-6 wow fadeInLeft " >
-                            <div className="about-item-content bg-white rounded px-4 h-100 d-flex align-items-center">
+                            <div className="bg-primary rounded p-5 h-100">
                                 <div>
-                                    <h2 className="mb-4 text-capitalize text-primary">Your All-in-One Digital Team—Powered by AVATAs</h2>
-                                    <p className='text-black-50'>From the comfort of your home or office, you can access a full team of remote digital experts—your personal AVATAs (Advanced Virtual Assistants in Technology and Advertising)—dedicated to helping you scale with ease.
+                                    <h2 className="mb-4 text-capitalize text-white">Your All-in-One Digital Team—Powered by AVATAs</h2>
+                                    <p className='text-black-100'>From the comfort of your home or office, you can access a full team of remote digital experts—your personal AVATAs (Advanced Virtual Assistants in Technology and Advertising)—dedicated to helping you scale with ease.
                                     </p>
-                                    <p className='text-black-50'>Whether you are an ambitious startup in Africa or a thriving brand aiming for global relevance, AVATAWorks is your all-in-one partner for digital brand success. We combine strategic branding, tech-driven creativity, and result-focused execution to deliver powerful marketing, design, and business growth solutions.
+                                    <p className='text-black-100'>Whether you are an ambitious startup in Africa or a thriving brand aiming for global relevance, AVATAWorks is your all-in-one partner for digital brand success. We combine strategic branding, tech-driven creativity, and result-focused execution to deliver powerful marketing, design, and business growth solutions.
                                     </p>
-                                    {/* <p className="text-dark"><i className="fa fa-check text-primary me-3"></i>We can save your money.</p>
-                                    <p className="text-dark"><i className="fa fa-check text-primary me-3"></i>Speed up production</p>
-                                    <p className="text-dark mb-4"><i className="fa fa-check text-primary me-3"></i>Our Our plans are flexible</p> */}
-                                    <Link href={'/about'}><PrimaryButton className='btn-danger py-2' text='meet our avatas' /></Link>
+                                    
+                                    <Link href={'/about'}><PrimaryButton className='btn-dark py-3 my-4' text='meet our avatas' /></Link>
                                 </div>
                             </div>
                         </div>
                         <div className="col-xl-6 wow fadeInRight">
-                            <div className="bg-white rounded p-5 p-sm-0 h-100">
-                                <div className="row g-4 justify-content-center w-100">
-                                    <div className="col-12">
-                                        <div className="">
-                                            <Image width={400} height={400} src="/aboutUs.jpeg" className="img-fluid w-100" alt="aboutUs" />
-                                        </div>
-                                    </div>
+                            <div className="bg-white rounded p-5 p-sm-0 h-100 mt-4">
+                                <div className="row g-4 justify-content-center w-100 p-4">
                                     <div className="col-12">
                                         <div className="counter-item bg-light rounded p-3 h-100">
                                             <div className="counter-counting">
--- src/components/homePage/Features.tsx
+++ src/components/homePage/Features.tsx
@@ -13,8 +13,7 @@ const Features = ({ featuresData, title }: FeaturesProps) => {
             <div className="container-fluid feature bg-light py-5">
                 <div className="container py-5">
                     <div className="text-center mx-auto pb-5 wow fadeInUp" style={{ maxWidth: '800px' }}>
-                        <h4 >Our Features</h4>
-                        <h2 className="text-primary mb-4">{title}</h2>
+                        <h2 className="text-primary">{title}</h2>
                         <p className="mb-0 p-4">At AVATAWorks, we don&apos;t just offer services—we deliver transformative solutions tailored to suite your brand needs. As your all-in-one Advanced Virtual Assistant in Technology and Advertising, we combine creativity, technology, and strategy to help brands scale faster, stronger, and smarter.
                         </p>
                     </div>
